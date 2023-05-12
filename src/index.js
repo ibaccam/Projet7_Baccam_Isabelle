29081446bc821ce92a44221c7feb529ae9293e2f
Isabelle Baccam <isabellebaccam@gmail.com>
2023-05-12 12:40:44 +0200

    Index.js Router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import "./styles/index.css";
-import Home from './pages/Home.jsx';
-import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Card from './pages/Card.jsx'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+
+
+ReactDOM.render(
   <React.StrictMode>
-    <Home />
-  </React.StrictMode>
-);
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <Router>
+      <Routes>
+        <Route path="/" element= {<Home />} errorElement = {<ErrorPage/>}/>
+        <Route path="/about" element= {<About/>} errorElement = {<ErrorPage />}/>
+        <Route path="/logement" element={<Card />} errorElement= {<ErrorPage />}/>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
