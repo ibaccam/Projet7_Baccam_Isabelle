d7b1f17f4b47930ef1c6d9224c61a4828385e348
Isabelle Baccam <isabellebaccam@gmail.com>
2023-05-15 17:58:49 +0200

    Page accueil-sass

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,7 +5,6 @@ function Cards({ image, title }) {
   return (
     <div className="card">
       <img src={image} alt="" className="card__img" />
-      <div className="card__filigrane"></div>
       <h2 className="card__title">{title}</h2>
     </div>
   )
