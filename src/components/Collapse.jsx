8f0388dff4be8a68c6d0688e19c37968bca389cd
Isabelle Baccam <isabellebaccam@gmail.com>
2023-05-16 16:29:26 +0200

    About-pages-sass

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import chevron_Up from "../assets/chevron_Up.png"
+import chevron_Down from "../assets/chevron_Down.png"
+
+
+function Collapse({title, content}) {
+    const [open, setOPen] = useState(false); // définissez notre état par défaut sur false (fermé)
+
+    const toggle = () => {
+        setOPen(!open);  // inverse la valeur actuelle
+      };
+
+        // défini les classes selon si c'est visible ou caché, idem pour le chevron
+        //const collapseContent = (open? "visible" : "hidden") + " collapse"
+
+        // permet de changer le sens du chevron si visible ou caché
+        const collapseChevron = (open ? chevron_Up : chevron_Down)
+      
+    return (
+        <div className="collapse">
+            <button type="button" className="collapse__button" onClick={toggle}>
+                <span className="collapse__title">{title}</span>
+                <div className="collapse__chevron">
+                    <img src={collapseChevron} alt="" className="collapse__img" />
+                </div>
+            </button> 
+            {open && (
+                <div className="collapse__content">
+                <div>{content}</div>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Collapse
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,32 @@
+import React from 'react'
+import Header from "../layout/Header"
+import Banner from "../components/Banner"
+import Footer from "../layout/Footer"
+import Collapse from "../components/Collapse"
+import bannerAbout from "../assets/banner_about.jpg"
+
+
 function About() {
-    return (
-      <div className="Home">
-        <header className="Home-header">
-          
-          <p>
-            Edit <code>src/pages/Home.jsx</code> and save to reload.
-          </p>
-          <a
-            className="Home-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    // contenu qui sera affiché dans les Collapse
+  const fiabilite = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes."
+  const respect   = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+  const service   = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
+  const securite  = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+  return (
+    <div className='about'>
+      <Header />
+      <div className="about__banner">
+        <Banner image={bannerAbout} />
       </div>
-    );
-  }
-  
-  export default About;
+      <div className='about__content'>
+        <div><Collapse title="Fiabilité" content={fiabilite} /></div>
+        <div><Collapse title="Respect" content={respect} /></div>
+        <div><Collapse title="Service" content={service} /></div>
+        <div><Collapse title="Sécurité" content={securite} /></div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default About
