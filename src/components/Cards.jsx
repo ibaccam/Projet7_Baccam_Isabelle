f251a59eee76960b65947e8274708229b9a62e55
Isabelle Baccam <isabellebaccam@gmail.com>
2023-05-25 13:32:41 +0200

    Components-Commentaires

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-// Cards récupère 2 propriétés => image & title
+// Cards récupère 2 props => image & title
 function Cards({ image, title }) {
   return (
     <div className="card">
